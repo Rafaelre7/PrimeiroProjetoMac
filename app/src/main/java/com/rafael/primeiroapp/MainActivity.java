6007edaf3dfd80444138a92d9ac898b7246d0c73
Rafael Pimenta <rafaelre7@gmail.com>
2020-09-08 05:13:01 -0300

    Primeiro commit

---
+++ app/src/main/java/com/rafael/primeiroapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.rafael.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnToast;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToast = findViewById(R.id.btnToast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Clicou", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
